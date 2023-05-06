03f8f620de12cee6481f62ee724b22028ebcb7bb
Nintasan <113663479+Nintasan@users.noreply.github.com>
2023-05-07 02:28:43 +0900

    save

--- public_html/app.py
+++ public_html/app.py
@@ -2,11 +2,15 @@ import sqlite3
 from flask import Flask , render_template , request , g , redirect , url_for , session , flash
 from datetime import timedelta
 from editpage import bp_editpage
+from flask_mail import Mail, Message
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
+
+mail =Mail(app)
 app.register_blueprint(bp_editpage)
 app.secret_key = "aaa"
 app.permanent_session_lifetime = timedelta(minutes=30)
+
 
 def get_db():
     if 'db' not in g:
@@ -14,8 +18,31 @@ def get_db():
     return g.db
 
 @app.route('/')
-def index():
+def home():
     return render_template('home.html', title = 'Vtuber art & rig comm【イラストレーター：ニンタ/HOME】')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title = 'Vtuber art & rig comm【イラストレーター：ニンタ/About】')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title = 'Vtuber art & rig comm【イラストレーター：ニンタ/Contact】')
+
+@app.route('/reply', methods=['POST'])
+def reply():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+
+    msg = Message(content,
+                  sender=(name, email),
+                  recipients=["ninta.illustration@gmail.com"])
+    mail.send(msg)
+
+
+    return render_template('reply.html', title = 'Vtuber art & rig comm【イラストレーター：ニンタ/Reply】')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ public_html/config.py
@@ -0,0 +1,7 @@
+MAIL_SERVER = '[メールアドレスの@以降]'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USE_SSL = False
+MAIL_USERNAME = '[メールアドレス]'
+MAIL_PASSWORD = '[パスワード]'
+MAIL_DEFAULT_SENDER = '[メールアドレス]'
