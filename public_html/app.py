016d1d3a8ba2b63f930b2b1610077cce641f1ec2
Nintasan <113663479+Nintasan@users.noreply.github.com>
2023-05-07 18:49:48 +0900

    save

--- public_html/app.py
+++ public_html/app.py
@@ -2,11 +2,9 @@ import sqlite3
 from flask import Flask , render_template , request , g , redirect , url_for , session , flash
 from datetime import timedelta
 from editpage import bp_editpage
-from flask_mail import Mail, Message
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__)
 
-mail =Mail(app)
 app.register_blueprint(bp_editpage)
 app.secret_key = "aaa"
 app.permanent_session_lifetime = timedelta(minutes=30)
@@ -17,9 +15,17 @@ def get_db():
         g.db = sqlite3.connect('nintahp.db')
     return g.db
 
+
 @app.route('/')
 def home():
-    return render_template('home.html', title = 'Vtuber art & rig comm【イラストレーター：ニンタ/HOME】')
+    con = get_db()
+    con.execute("CREATE TABLE IF NOT EXISTS contents (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, content NOT NULL)"
+        )
+    c = con.cursor()
+    cur = c.execute("select * from contents ORDER BY id")
+    content = cur.fetchall()
+    con.close()
+    return render_template('home.html', title = 'Vtuber art & rig comm【イラストレーター：ニンタ/HOME】', content = content)
 
 @app.route('/about')
 def about():
@@ -27,21 +33,53 @@ def about():
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html', title = 'Vtuber art & rig comm【イラストレーター：ニンタ/Contact】')
+    con = get_db()
+    con.execute("CREATE TABLE IF NOT EXISTS contents (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, content NOT NULL)"
+        )
+    c = con.cursor()
+    cur = c.execute("select * from contents ORDER BY id")
+    content = cur.fetchall()
+    con.close()
+    return render_template('contact.html', title = 'Vtuber art & rig comm【イラストレーター：ニンタ/Contact】', content = content)
 
-@app.route('/reply', methods=['POST'])
+@app.route('/reply')
 def reply():
+    return render_template('reply.html', title = 'Vtuber art & rig comm【イラストレーター：ニンタ/Reply】')
+
+@app.route('/editnintahp')
+def editnintahp():
+    return render_template('editpage.html', title = 'Vtuber art & rig comm【イラストレーター：ニンタ/Edit】')
+
+@app.route('/editnintahpsend', methods=['POST'])
+def editnintahpsend():
     name = request.form['name']
-    email = request.form['email']
     content = request.form['content']
 
-    msg = Message(content,
-                  sender=(name, email),
-                  recipients=["ninta.illustration@gmail.com"])
-    mail.send(msg)
+    con = get_db()
+    con.execute(
+            "INSERT INTO contents (name, content) VALUES (?, ?)",
+            [name, content]
+        )
+    con.commit()
+    con.close()
+
+    return redirect(url_for('home'))
+
+@app.route('/editnintahpedit', methods=['POST'])
+def editnintahpedit():
+    id = request.form['id']
+    name = request.form['name']
+    content = request.form['content']
+
+    con = get_db()
+    con.execute("UPDATE contents SET content = (?) WHERE id = (?)",
+                [content,id]
+                )
+    con.commit()
+    con.close()
+    return redirect(url_for('home'))
 
 
-    return render_template('reply.html', title = 'Vtuber art & rig comm【イラストレーター：ニンタ/Reply】')
 
 
 if __name__ == '__main__':
